377ee3421fa005749437894e2db81b0f660837de
deewanstudios <daniel@deewanstudios.com>
2014-12-18 20:15:20 +0000

    ytr

---
+++ application/controllers/name_controller.php
@@ -0,0 +1,23 @@
+<?php	// contact page controller
+
+// include the title for the page
+$title = "Contact";
+
+// include the data page
+include DATA."name.php";
+
+// include the header
+include PAGES."includes/header.php";
+
+
+// include the page content
+include PAGES."name/name.php";
+
+
+// include the footer
+include PAGES."includes/footer.php";
+
+
+
+
+?>
