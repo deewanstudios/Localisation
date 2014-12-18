c7e31fb08cc124cceacd22617ae873180d66cc9a
deewanstudios <daniel@deewanstudios.com>
2014-12-18 19:34:04 +0000

    updating

---
+++ application/pages/sparks/sparks.php
@@ -0,0 +1,76 @@
+
+<div id="middle">
+	
+    <div class="middle-part-in">
+        <div class="wrapper-1">
+            <!-- <?php
+            echo "$content";
+
+            ?> -->
+            <h1 class="h1-title"><?php print $content ?></h1>
+            <div class="menu-div">
+                <div class="core-business">
+                    <h4>Core Business</h4>
+                    Back in 2009 we set out with a simple mission;
+                    <br>
+                    To help small businesses and organisations get their goods delivered.
+                    <br>
+                    But we do much, much more…
+                </div>
+                <div id="menu">
+                    <ul>
+                        <li>
+                            <a href="#" title="Collection &amp; Delivery" class="active" rel="For both domestic and business customers,
+                            we can collect and deliver single or multiple items. Whether it's urgent, same day, or overnight deliveries -
+                            we have a delivery service to suit your needs."
+                            rev="site/images/service.jpg">Collection &amp; Delivery</a>
+                        </li>
+                        <li>
+                            <a href="#" title="Bulky Items" rel="Many courier companies won't touch your heavy or bulky items. 
+                            Whether it's a sofa or jacuzzi, we have a 1 or 2 man courier service to help you carry your item in and out of your home, or up and down stairs." 
+                            rev="site/images/bulkyitems.jpg">Bulky Items</a>
+                        </li>
+                        <li>
+                            <a href="#" title="Valuable &amp; Fragile Items" rel="When it comes to your prized possessions, you need to trust the people who are transporting them. 
+                            Our meticulous couriers will make sure your package arrives in perfect condition. We are insured to transport items up to £20,000."
+                             rev="site/images/Fragile Parcel.jpg">Valuable &amp; Fragile Items</a>
+                        </li>
+                        <li>
+                            <a href="#" title="Multi-Drop Deliveries" rel="We provide a tailor made service for those who need regular deliveries at great prices." 
+                            rev="site/images/multidrop.jpg">Multi-Drop Deliveries</a>
+                        </li>
+                        <li>
+                            <a href="#" title="Nationwide Courier" rel="From Lands End to John O'Groats we travel across the country delivering items for our customers." 
+                            rev="site/images/nationwide.jpg">Nationwide Courier</a>
+                        </li>
+                        <li>
+                            <a href="#" title="Man &amp; Van" rel="Hire our men and vans at competitive prices. From £30 an hour you can have full use of our hard working team." 
+                            rev="site/images/manvan.jpg">Man &amp; Van</a>
+                        </li>
+                        <li>
+                            <a href="#" title="House Moves" rel="Let us take the stress out of moving home, your items will arrive in perfect condition." 
+                            rev="site/images/housemoves.jpg">House Moves</a>
+                        </li>
+                        <li>
+                            <a href="#" title="Office Moves" rel="Whether you need documents taken into storage or you're simply relocating." 
+                            rev="site/images/officemoves.jpg">Office Moves</a>
+                        </li>
+                    </ul>
+                </div>
+                <div id="main">
+                    <div class="main-img"><img id="my_image" src="site/images/service.jpg" alt="" title="">
+                    </div>
+                    <div class="box-div">
+                        <h3>Collection &amp; Delivery</h3>
+                        <h5>For both domestic and business customers, we can collect and deliver single or multiple items. Whether it's urgent, same day, 
+                        	or overnight deliveries - we have a delivery service to suit your needs.</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+    <!--  middle-part-in-->
+    <div class="clear"></div>
+
+</div>
